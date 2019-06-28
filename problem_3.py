8753d1fe3a8996225b65dd3add904211ec09f11d
bsantos2 <bsantos2@gmail.com>
2019-06-28 09:11:41 -0700

    problem 3
    I did not use sort function but append and insert, and only went
    through the array in a for loop once.

---
+++ problem_3.py
@@ -0,0 +1,30 @@
+def sort_012(input_list):
+    """
+    Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal.
+
+    Args:
+       input_list(list): List to be sorted
+    """
+    bin0 = list()
+    index_for_1 = 0
+    for i in range(0, len(input_list)):
+        if input_list[i] == 0:
+            bin0.insert(0, input_list[i])
+            index_for_1 += 1
+        elif input_list[i] == 2:
+            bin0.append(input_list[i])
+        else:
+            bin0.insert(index_for_1, input_list[i])
+    return bin0
+
+def test_function(test_case):
+    sorted_array = sort_012(test_case)
+    print(sorted_array)
+    if sorted_array == sorted(test_case):
+        print("Pass")
+    else:
+        print("Fail")
+
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
